081b6a5c9156a8892d51c50cee4850305fe725c0
Techcable <git@techcable.net>
2026-01-05 15:07:33 -0700

    Revert "Implement bytemuck::NoUninit for StdThreadId"
    Not guaranteed to be safe, and for this reason I am yanking v0.1.2.
    If Atomic<ThreadId> is needed, use threadid::UniqueThreadId.
    
    This reverts commit 051358db2464a863c120514037b2b642b8926b13.

--- src/std.rs
+++ src/std.rs
@@ -18,6 +18,15 @@ fast_thread_local! {
 /// Using [`StdThreadId::current`] provides fast access to the current identifier.
 /// It is significantly faster than using [`std::thread::current`],
 /// which requires cloning a [`std::sync::Arc`].
+///
+/// # Limitations
+/// This type cannot implement [`bytemuck::NoUninit`] as the stdlib's representation is not guaranteed.
+/// That means that this type cannot be used inside an [`atomic::Atomic`].
+/// If that is needed, use [`UniqueThreadId`] (possibly with the `unique-wrap-std` feature).
+///
+/// [`bytemuck::NoUninit`]: https://docs.rs/bytemuck/1/bytemuck/trait.NoUninit.html
+/// [`atomic::Atomic`]: https://docs.rs/atomic/0.6/atomic/struct.Atomic.html
+/// [`UniqueThreadId`]: crate::UniqueThreadId
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "nightly-docs", doc(cfg(feature = "std")))]
 #[must_use]
@@ -55,14 +64,6 @@ unsafe impl crate::IThreadId for StdThreadId {
 #[cfg_attr(feature = "nightly-docs", doc(cfg(feature = "bytemuck")))]
 // SAFETY: We are #[repr(transparent)]
 unsafe impl bytemuck::TransparentWrapper<ThreadId> for StdThreadId {}
-
-#[cfg(feature = "bytemuck")]
-#[cfg_attr(feature = "nightly-docs", doc(cfg(feature = "bytemuck")))]
-// SAFETY: We trust that std::thread::ThreadId just wraps an integer
-// Technically this isn't guaranteed by the API,
-// but there is no other reasonable implementation.
-unsafe impl bytemuck::NoUninit for StdThreadId {}
-
 // SAFETY: stdlib guarantees that threadid is unique
 unsafe impl crate::IThreadId for ThreadId {
     #[inline]
